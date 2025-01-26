3ca53ee5057e9084c59c45a2182e4812d1881889
Mandy <hokamandy2@gmail.com>
2025-01-26 13:59:05 +0800

    My answer

--- bonus.py
+++ bonus.py
@@ -7,6 +7,16 @@ return the length of the longest palindrome that can be built with those letters
 For this question, letters are case-sensitive, for example, "Aa" is not considered a palindrome.
 """
 # Change this function so it works correctly
+def longest_palindrome_length(text):
+    original = set()
+    repeated = set()
+    for letter in text:
+        if letter in original:
+            repeated.add(letter)
+        else:
+            original.add(letter)
+    length = len(text) - len(set(text)) + len(repeated) + 1
+    return length
 
 if __name__ == '__main__':
     test_cases = [('abccccdd', 7),      # dccaccd / dccbccd
